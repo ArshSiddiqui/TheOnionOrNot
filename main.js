2ada19d3e2059d12dc128799d120be74471e80a3
Arsh Siddiqui <ArshSiddiqui@users.noreply.github.com>
2021-09-23 09:44:28 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,15 +2,12 @@ var file = new XMLHttpRequest()
 var text = []
 file.open("GET", OnionArircles.txt, false);
 file.onreadystatechange = function(){
-   if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
+   if(rawFile.readyState === 4){
+            if(rawFile.status === 200 || rawFile.status == 0){
                 var allText = rawFile.responseText;
                 alert(allText);
                 text = allText.split("\n");
             }
-        }
     }
     rawFile.send(null);
 }
