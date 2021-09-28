8e8e95bddb623f928e8bb8cf40cf48dd4d788a8f
arshsiddiqui <arshsiddiqui@Vt.edu>
2021-09-28 10:10:05 -0400

    Changes

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
-// var file = new XMLHttpRequest()
-// var text = []
-// file.open("GET", OnionArircles.txt, false);
-// file.onreadystatechange = function(){
-//    if(rawFile.readyState === 4){
-//             if(rawFile.status === 200 || rawFile.status == 0){
-//                 var allText = rawFile.responseText;
-//                 alert(allText);
-//                 text = allText.split("\n");
-//             }
-//     }
-//     rawFile.send(null);
-// }
+//var file = new XMLHttpRequest()
+//var text = []
+//file.open("GET", "http://OnionAricles.txt", false);
+//console.log("here")
+//file.onreadystatechange = function(){
+//   if(rawFile.readyState === 4){
+//            if(rawFile.status === 200 || rawFile.status == 0){
+//                var allText = rawFile.responseText;
+//                alert(allText);
+//                text = allText.split("\n");
+//            }
+//    }
+//    rawFile.send(null);
+//}
+//console.log(text)
+//var filec= new XMLHttpRequest()
+//var text = []
+//filec.open("GET", "OnionOrNot.csv", false);
+//var data = $.csv.toObjects(filec)
+//console.log(data)
+//var randNum = Math.floor(Math.random() * 1000);
+//document.getElementById("artitle").innerHTML = data[randNum]
+//var randNum = Math.floor(Math.random() * 1000);
+//document.getElementById("artitle").innerHTML = text[randNum]
 
-var filec= new XMLHttpRequest()
-var text = []
-filecopen("GET", OnionOrNot.csv, false);
-var data = $.csv.toObjects(filec)
-console.log(data)
-var randNum = Math.floor(Math.random() * 1000);
-document.getElementById("artitle").innerHTML = data[randNum]
+console.log("woow")
